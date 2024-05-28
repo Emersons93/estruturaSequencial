0f61e3177c51f77a5c0397f1c3ee365d09577678
Emerson Soares da Silva <esds.emerson1993@gmail.com>
2024-05-28 15:51:49 -0300

    exercicio ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto                 de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).

---
+++ src/quartoEx.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class quartoEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B, C, D, dif;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+
+        dif = A * B - C * D;
+
+        System.out.println("DIFERENCA = " + dif);
+        sc.close();
+
+    }
+}
